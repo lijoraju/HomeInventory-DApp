ce0fec14c4a7290c1b32c42d292c851c28f292fc
Lijo <lijoraju37@gmail.com>
2023-08-26 09:00:40 +0530

    implemented auction functionality

--- smartcontract/HomeInventory.sol
+++ smartcontract/HomeInventory.sol
@@ -18,9 +18,20 @@ contract HomeInventory {
         uint256 id;
     }
 
+    struct Auction {
+        uint256 itemId;
+        address highestBidder;
+        uint256 highestBid;
+        uint256 basePrice;
+        uint256 endTime;
+        uint256 depositedAmount; // Deposited amount for the highest bid
+    }
+
     mapping(uint256 => Item) public items;
-    mapping(uint256 => address[]) public ownershipHistory; // Ownership history mapping
+    mapping(uint256 => address[]) public ownershipHistory;
+    mapping(uint256 => Auction) public auctions;
     uint256 public nextItemId = 1;
+    uint256 public nextAuctionId = 1;
 
     constructor() {
         owner = msg.sender;
@@ -57,8 +68,89 @@ contract HomeInventory {
         });
 
         items[nextItemId] = newItem;
-        ownershipHistory[nextItemId].push(msg.sender); // Record initial owner
+        ownershipHistory[nextItemId].push(msg.sender);
         nextItemId++;
+    }
+
+    function startAuction(uint256 _itemId, uint256 _basePrice, uint256 _durationInHours) external onlyOwner {
+        require(_itemId > 0 && _itemId < nextItemId, "Invalid item ID");
+        require(auctions[_itemId].endTime == 0, "Auction already started");
+
+        auctions[_itemId] = Auction({
+            itemId: _itemId,
+            highestBidder: address(0),
+            highestBid: 0,
+            basePrice: _basePrice,
+            endTime: block.timestamp + _durationInHours * 1 hours,
+            depositedAmount: 0
+        });
+    }
+
+    function placeBid(uint256 _itemId, uint256 _bidAmount) external payable {
+        require(_itemId > 0 && _itemId < nextItemId, "Invalid item ID");
+        Auction storage auction = auctions[_itemId];
+        require(auction.endTime > 0, "Auction not started");
+        require(auction.endTime > block.timestamp, "Auction ended");
+        require(_bidAmount > auction.highestBid, "Bid amount must be higher than current highest bid");
+        require(msg.value == _bidAmount, "Bid amount must be sent with the transaction");
+
+        // Refund previous bidder
+        if (auction.highestBidder != address(0)) {
+            address payable previousBidder = payable(auction.highestBidder);
+            previousBidder.transfer(auction.depositedAmount); // Refund the previous bidder
+        }
+
+        auction.highestBidder = msg.sender;
+        auction.highestBid = _bidAmount;
+        auction.depositedAmount = _bidAmount; // Deposit the bid amount
+    }
+
+    function finalizeAuction(uint256 _itemId) external {
+        require(_itemId > 0 && _itemId < nextItemId, "Invalid item ID");
+        Auction storage auction = auctions[_itemId];
+        require(auction.endTime > 0, "Auction not started");
+        require(auction.endTime <= block.timestamp, "Auction not ended");
+        require(auction.highestBidder != address(0), "No bids received");
+
+        Item storage item = items[_itemId];
+        address payable winningBidder = payable(auction.highestBidder); // Convert to payable address
+        uint256 depositedAmount = auction.depositedAmount;
+
+        // Transfer ownership and remove from auction
+        item.owner = winningBidder;
+        delete auctions[_itemId];
+
+        // Transfer deposited amount to new owner (current owner)
+        winningBidder.transfer(depositedAmount);
+
+        ownershipHistory[_itemId].push(winningBidder);
+    }
+
+    function getAuctionDetails(uint256 _itemId) external view returns (Auction memory) {
+        require(_itemId > 0 && _itemId < nextItemId, "Invalid item ID");
+        return auctions[_itemId];
+    }
+
+    function getAuctionItems() external view returns (Auction[] memory) {
+        uint256 itemCount = 0;
+
+        for (uint256 i = 1; i < nextItemId; i++) {
+            if (auctions[i].endTime > 0 && auctions[i].endTime > block.timestamp) {
+                itemCount++;
+            }
+        }
+
+        Auction[] memory auctionItems = new Auction[](itemCount);
+        itemCount = 0;
+
+        for (uint256 i = 1; i < nextItemId; i++) {
+            if (auctions[i].endTime > 0 && auctions[i].endTime > block.timestamp) {
+                auctionItems[itemCount] = auctions[i];
+                itemCount++;
+            }
+        }
+
+        return auctionItems;
     }
 
     function getItem(uint256 _id) external view returns (Item memory) {
